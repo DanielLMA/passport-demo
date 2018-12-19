b3de1568b98dcceb6911fef1296350466b40d1f1
Flynn <flynnwebdev@gmail.com>
2018-12-19 22:53:24 +1000

    Authorization added

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Passport Demo', user: req.user });
 });
+
+router.get('/foo', function (req, res, next) {
+  res.json(req.user)
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,29 +5,22 @@ const router = express.Router();
 
 // Register a new user
 router.post('/register', (req, res) => {
-  User.register(new User({ email: req.body.email }), req.body.password, (err) => {
+  User.register(new User({ email: req.body.email, role: 'user' }), req.body.password, (err) => {
     if (err) {
       return res.status(500).send(err.message);
     }
     // Log the new user in (Passport will create a session) using the local strategy
     passport.authenticate('local')(req, res, () => {
-      // req.user exists at this point.
-      // Normally we wouldn't send back the entire user object - this is for learning purposes.
-      // Instead, we might send back only the username or email, or even just a status code.
-      res.json(req.user)
-      // res.sendStatus(200)
+      req.session.role = req.user.role || 'user'
+      res.sendStatus(200)
     });
   });
 });
 
 // Login an existing user
 router.post('/login', passport.authenticate('local'), (req, res) => {
-  // At this point, authentication was successful and req.user exists.
-
-  // Normally we wouldn't send back the entire user object - this is for learning purposes.
-  // Instead, we might send back only the username or email, or even just a status code.
-  res.json(req.user)
-  // res.sendStatus(200)
+  req.session.role = req.user.role || 'user'
+  res.sendStatus(200)
 });
 
 // Logout the current user
